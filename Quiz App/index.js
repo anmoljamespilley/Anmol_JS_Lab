425e0654dfb5d783f6ab46ada1fce52e92cab365
Anmol Pilley <144140402+anmoljamespilley@users.noreply.github.com>
2024-04-11 22:40:17 +0530

    index.js

--- Quiz App/index.js
+++ Quiz App/index.js
@@ -2,99 +2,119 @@ class Quiz {
     constructor(questions) {
         this.score = 0;
         this.questions = questions;
-        this.questionIndex = 0;
+        this.currentQuestionIndex = 0;
     }
 
     get currentQuestion() {
-        return this.questions[this.questionIndex];
+        return this.questions[this.currentQuestionIndex];
     }
 
-    checkAnswer(answer) {
-        if (this.currentQuestion.answer === answer) {
+    answerQuestion(answer) {
+        if (this.currentQuestion.isCorrect(answer)) {
             this.score++;
         }
         this.moveToNextQuestion();
     }
 
     moveToNextQuestion() {
-        this.questionIndex++;
+        this.currentQuestionIndex++;
     }
 
     isEnded() {
-        return this.questionIndex === this.questions.length;
+        return this.currentQuestionIndex === this.questions.length;
+    }
+
+    getScore() {
+        return this.score;
+    }
+
+    getPercentage() {
+        return (this.score / this.questions.length) * 100;
     }
 }
 
 class Question {
-    constructor(text, options, answer) {
+    constructor(text, options, correctOptionIndex) {
         this.text = text;
         this.options = options;
-        this.answer = answer;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    isCorrect(answerIndex) {
+        return answerIndex === this.correctOptionIndex;
     }
 }
 
 class QuizUI {
     constructor(quiz) {
         this.quiz = quiz;
+        this.quizElem = document.getElementById("quiz");
+        this.questionElem = document.getElementById("question");
+        this.progressElem = document.getElementById("progress");
+        this.attachButtonListeners();
+        this.displayQuestion();
     }
 
     displayQuestion() {
-        const quizElem = document.getElementById("quiz");
         if (this.quiz.isEnded()) {
-            this.showScores(quizElem);
+            this.showScores();
         } else {
-            const questionElem = document.getElementById("question");
-            questionElem.innerText = this.quiz.currentQuestion.text;
-
-            const choices = this.quiz.currentQuestion.options;
-            choices.forEach((choice, index) => {
-                const choiceElem = document.getElementById(`choice${index}`);
-                choiceElem.innerText = choice;
-                this.handleClickOnBtn(`btn${index}`, choice);
-            });
-
-            this.showProgress(quizElem);
+            const question = this.quiz.currentQuestion;
+            this.questionElem.innerText = question.text;
+            this.displayOptions(question.options);
+            this.showProgress();
         }
     }
 
-    showProgress(quizElem) {
-        const progressElem = quizElem.querySelector("#progress");
-        progressElem.innerText = `Question ${this.quiz.questionIndex + 1} of ${this.quiz.questions.length}`;
+    displayOptions(options) {
+        const choices = Array.from(this.quizElem.querySelectorAll(".buttons button span"));
+        choices.forEach((choiceElem, index) => {
+            choiceElem.innerText = options[index];
+        });
     }
 
-    handleClickOnBtn(id, choice) {
-        const buttonElem = document.getElementById(id);
-        const quiz = this.quiz;
-        buttonElem.onclick = function () {
-            quiz.checkAnswer(choice);
-            quizUI.displayQuestion();
-        };
+    showProgress() {
+        this.progressElem.innerText = `Question ${this.quiz.currentQuestionIndex + 1} of ${this.quiz.questions.length}`;
     }
 
-    showScores(quizElem) {
-        const result = `<h1>Result</h1><h2 id="score">Thank you! Here are your results: <br>Score: ${this.quiz.score}/${this.quiz.questions.length} <br> Marks percentage: ${(this.quiz.score / this.quiz.questions.length) * 100}% </h2>`;
-        quizElem.innerHTML = result;
+    attachButtonListeners() {
+        const buttons = Array.from(this.quizElem.querySelectorAll(".buttons button"));
+        buttons.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                this.quiz.answerQuestion(index);
+                this.displayQuestion();
+            });
+        });
+    }
+
+    showScores() {
+        const result = `
+            <h1>Result</h1>
+            <h2 id="score">Thank you! Here are your results: <br>
+            Score: ${this.quiz.getScore()}/${this.quiz.questions.length} <br>
+            Marks percentage: ${this.quiz.getPercentage()}%
+            </h2>`;
+        this.quizElem.innerHTML = result;
     }
 }
 
 const questions = [
     new Question("In CSS, which property is used to change the color of text?",
                 ["background-colour", "text-colour", "colour", "font-colour"],
-                "colour"),
+                2),
     new Question("Which keyword is used to define a class in Java?",
                 ["class", "void", "int", "new"],
-                "class"),
+                0),
     new Question("What is the purpose of SQL 'INSERT INTO' statement?",
                 ["To delete data from a table", "To update existing data", "To add new data into a table", "To select data from a table"],
-                "To add new data into a table"),
+                2),
     new Question("What is the result of 5 + 7 * 2 in most programming languages?",
                 ["24", "19", "26", "21"],
-                "19"),
+                1),
     new Question("What does CSS stand for?",
                 ["Cascading Style Sheets", "Creative Style Syntax", "Colorful Style System", "Computer Style Sheets"],
-                "Cascading Style Sheets")
+                0)
 ];
 
 const quiz = new Quiz(questions);
 const quizUI = new QuizUI(quiz);
-quizUI.displayQuestion();
